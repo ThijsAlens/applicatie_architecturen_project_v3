625fe39bf44061f2369f76e66d7021f6109970a8
Ubuntu - Thijs Alens <thijs.alens@telenet.be>
2025-01-03 15:28:34 +0100

    first commit

---
+++ klusje_v3/src/main/java/klusje_v3/KlusjeV3Application.java
@@ -0,0 +1,13 @@
+package klusje_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlusjeV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlusjeV3Application.class, args);
+	}
+
+}
---
+++ klusje_v3/src/test/java/klusje_v3/KlusjeV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package klusje_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlusjeV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
