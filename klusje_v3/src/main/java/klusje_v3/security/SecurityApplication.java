118f619c476d35f3773a0c83eccb6da9f640ab5c
Ubuntu - Thijs Alens <thijs.alens@telenet.be>
2025-01-03 16:09:32 +0100

    basic nav

---
+++ klusje_v3/src/main/java/klusje_v3/security/SecurityApplication.java
@@ -0,0 +1,12 @@
+package klusje_v3.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
